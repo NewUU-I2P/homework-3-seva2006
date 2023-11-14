9de49f98b51d3574570c20b114feb5e2270e1387
seva2006 <147371793+seva2006@users.noreply.github.com>
2023-11-14 17:02:10 +0300

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -5,7 +5,7 @@ std::string problemSolution4(const std::string &macAddress) {
     std::string result;
       int counter = 0;
       for (int i = 0; i < 18; i += 3) {
-          if (macAddress[i] == 'F' && macaddress[i + 1] == 'F') {
+          if (macAddress[i] == 'F' && macAddress[i + 1] == 'F') {
               counter += 2;
               break;
           }
@@ -14,14 +14,19 @@ std::string problemSolution4(const std::string &macAddress) {
               break;
           }
       }
-      if (isdigit(macAddress[1]) == true) {
+      if (is digit(macAddress[1]) == true) {
           if (macAddress[1] % 2 == 0) {
-          } else {
+              result="Unicast";
+          }
+          else {
               result = "Multicast";
           }
-      } else if (int(macAddress[1] + 1) % 2 == 0) {
+      }
+      else if (int(macAddress[1] + 1) % 2 == 0) 
+      {
           result = "Unicast";
-      } else {
+      } 
+      else {
           result = "Multicast";
       }
     return result;
