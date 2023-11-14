8c3f1cb75fca155c3f6317769f55d803791fa717
seva2006 <147371793+seva2006@users.noreply.github.com>
2023-11-14 18:44:07 +0300

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -2,32 +2,20 @@
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
-    std::string result;
-      int counter = 0;
-      for (int i = 0; i < 18; i += 3) {
-          if (macAddress[i] == 'F' && macAddress[i + 1] == 'F') {
-              counter += 2;
-              break;
-          }
-          if (counter == 12) {
-              result = "broadcast";
-              break;
-          }
-      }
-      if (isdigit(macAddress[1]) == true) {
-          if (macAddress[1] % 2 == 0) {
-              result="Unicast";
-          }
-          else {
-              result = "Multicast";
-          }
-      }
-      else if (int(macAddress[1] + 1) % 2 == 0) 
-      {
-          result = "Unicast";
-      } 
-      else {
-          result = "Multicast";
-      }
-    return result;
+std::stringstream ss(macAddress);
+std::string hexNumber;
+int firstOctet;
+
+std::getline(ss, hexNumber, ':');
+std::stringstream(hexNumber)>>std::hex>> firstOctet;
+
+std::string addressType;
+if (firstOctet % 2 == 0){
+   addressType="Unicast";
+}else if (firstOctet==0xFF){
+   addressType="Broadcast";
+}else{
+   addressType="Multicast";
+}
+return 0;
 }
