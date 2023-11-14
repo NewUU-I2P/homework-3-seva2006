147270e67b0039ff72f52ad82f4747affbfd215f
seva2006 <147371793+seva2006@users.noreply.github.com>
2023-11-14 16:29:04 +0300

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,5 @@
 float problemSolution2(float i, float j, float k) {
     float result;
-     result=(i > j && i > k)? "The maximum number is ":
-      (j > i && j > k) ? "The maximum number is ":
-         "The maximum number is ";
+     result=(i > j && i > k)? i : (j > i && j > k) ? j : k;
     return result;
 }
