3996ba4a1d05185b36bd1bf1abac08c370713e49
seva2006 <147371793+seva2006@users.noreply.github.com>
2023-11-14 19:00:04 +0300

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,24 +1,23 @@
 #include <string>
-
 std::string problemSolution3(float height, char S){
-std:: string result;
+   std:: string result;
    switch (S) {
         case 'M':
             if (height < 1.70) {
-                result = 'Short';
+                result = "Short";
             } else if (height>=1.70&&height < 1.85) {
-                result = 'Normal';
-            } else if (height >= 1.85){
-                result = 'Tall';
+                result = "Normal";
+            } else {
+                result = "Tall";
             }
             break;
         case 'F':
             if (height<1.60) {
-                result = 'Short';
+                result = "Short";
             }else if(height>1.60&&height <1.75) {
-                  result = 'Normal';
-            }else if (height>=1.75){
-                result = 'Tall';
+                  result = "Normal";
+            }else {
+                result = "Tall";
             }
             break;
     return result;
